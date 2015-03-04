fec1f4a20bc0fe55356ee89364e8eb01d29998bb
Sean Hudson <sean_hudson@mentor.com>
2015-03-03 19:36:01 -0600

    [u-boot] Add logbuff_printf function
    Signed-off-by: Sean Hudson <sean_hudson@mentor.com>

--- common/cmd_log.c
+++ common/cmd_log.c
@@ -38,6 +38,7 @@ static void logbuff_puts(const char *s);
 static int logbuff_printk(const char *line);
 
 static char buf[1024];
+static char buf2[1024];
 
 /* This combination will not print messages with the default loglevel */
 static unsigned console_loglevel = 3;
@@ -305,4 +306,14 @@ static int logbuff_printk(const char *line)
 			msg_level = -1;
 	}
 	return i;
+}
+
+void logbuff_printf ( const char *fmt, ... )
+{
+	va_list	args;
+
+	va_start(args, fmt);
+	vsnprintf(buf2, sizeof(buf2), fmt, args);
+	va_end(args);
+	logbuff_printk(buf2);
 }
